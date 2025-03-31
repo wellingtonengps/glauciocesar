53a2d6416d761ffecd6872cee40f175198da7d87
Wellington Pereira <43380060+wellingtonengps@users.noreply.github.com>
2025-03-31 17:53:48 -0300

    gh-page

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // Gera arquivos estáticos para GitHub Pages
-    basePath: process.env.NEXT_PUBLIC_BASE_PATH || '', // Ajusta o caminho base
-    assetPrefix: process.env.NEXT_PUBLIC_SITE_URL || '', // Para assets (CSS, JS, imagens)
+    output: 'export',
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+    assetPrefix: process.env.NEXT_PUBLIC_SITE_URL || '',
     images: {
-        unoptimized: true, // Desativa otimização de imagens (obrigatório no `next export`)
+        unoptimized: true,
     },
 };
 
